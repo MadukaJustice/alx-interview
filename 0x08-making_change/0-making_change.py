cfc8a8d35619a0f4d76dd0e430d1b30fdfd69f22
MadukaJustice <jonasjustice98@gmail.com>
2023-08-24 08:57:50 +0000

    Python change making algorithm (shortest route)

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,17 +5,25 @@
 
 
 def makeChange(coins, total):
-    """ Function definition: Solution using Dynamic Programming """
-    # initialize values with a very high number(infinity)
-    ways = [float('inf')] * (total + 1)
-    ways[0] = 0
+    """makeChange implementation with greedy algorithm
 
+    Returns:
+        - fewest number of coins if total can be met
+        - 0 if total is 0 or less
+        - -1 if total cannot be met by any number of coins
+    """
+    if total <= 0:
+        return 0
+
+    coins = sorted(coins, reverse=True)
+    num_coins = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            if ways[i - coin] != float('inf'):
-                ways[i] = min(ways[i], ways[i - coin] + 1)
+        if total <= 0:
+            break
+        num_coins += total // coin
+        total = total % coin
 
-    if ways[total] == float('inf'):
+    if total != 0:
         return -1
 
-    return ways[total]
+    return num_coins
