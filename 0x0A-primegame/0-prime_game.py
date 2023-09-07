b6822e88eeb669dfaed27c90ee183f4b57fb4fbe
MadukaJustice <jonasjustice98@gmail.com>
2023-09-07 19:23:49 +0000

    Prime algorithm (using grundy number)

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-""" Prime game algo """
+"""
+Prime Game
+"""
 
 
-def calculate_highest_power_of_two(n):
-    """ Calculates the highest power of 2 that divides a number n """
-    power_of_two = n
-    while power_of_two & (power_of_two - 1) != 0:
-        power_of_two = power_of_two & (power_of_two - 1)
-    return power_of_two
+def get_prime_numbers(n):
+    """
+    Generates prime numbers less than or equal to n using the Sieve of
+    Eratosthenes
+    """
+    sieve = [True] * (n + 1)
+    for i in range(2, int(n**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+
+    return [i for i in range(2, n + 1) if sieve[i]]
 
 
 def isWinner(x, nums):
-    """ Determines the winner of the game """
-    nimber = 0  # or grundy number
+    """
+    runs the game for the specified rounds in nums array
+    """
+    invalid_nums = type(nums) is not list or len(nums) == 0 or any(
+        type(n) is not int or n < 0 for n in nums)
+    invalid_x = type(x) is not int or x != len(nums)
+    if invalid_nums or invalid_x:
+        return None
 
-    for n in nums:
-        if n % 2 == 0:
-            nimber ^= 0
-        else:
-            highest_power_of_two = calculate_highest_power_of_two(n)
-            nimber ^= highest_power_of_two
+    if x == 1 and nums[0] == 1:
+        return 'Ben'
 
-    return "Ben" if nimber > 0 else "Maria"
+    primes = get_prime_numbers(max(nums))
+    if len(primes) == 0:
+        return None
+
+    stats = [len(list(filter(lambda x: x <= n, primes))) % 2 for n in nums]
+    maria_wins = sum(stats)
+    if maria_wins == len(stats) / 2:
+        return None
+    if maria_wins > len(stats) / 2:
+        return 'Maria'
+    else:
+        return 'Ben'
